de284c8fb657c353fcc159e7ec6ae052d47b64df
Dhiti <83868722+Dhiti@users.noreply.github.com>
2021-11-13 14:01:41 +0530

    Add files via upload

---
+++ Project#93/kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBrlCsEQD_wCzQ-gQ52f8BCLg-jrrML7lE",
+    authDomain: "let-schat-fb837.firebaseapp.com",
+    databaseURL: "https://let-schat-fb837-default-rtdb.firebaseio.com",
+    projectId: "let-schat-fb837",
+    storageBucket: "let-schat-fb837.appspot.com",
+    messagingSenderId: "464264005213",
+    appId: "1:464264005213:web:90882ae90dbe56ac922fb4"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
